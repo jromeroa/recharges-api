e1f12b8f1f20ff105f6aa8e3f35aa32122418ca2
Jhonny Romero <jromeroarenales@gmail.com>
2023-10-09 19:25:12 -0500

    solution to Spring Boot CORS in case the API is accessed from a local front

--- src/main/java/com/jhonny/rechargesapi/RechargesApiApplication.java
+++ src/main/java/com/jhonny/rechargesapi/RechargesApiApplication.java
@@ -2,6 +2,9 @@ package com.jhonny.rechargesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RechargesApiApplication {
@@ -10,4 +13,13 @@ public class RechargesApiApplication {
 		SpringApplication.run(RechargesApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
--- src/main/java/com/jhonny/rechargesapi/Sale/Sale.java
+++ src/main/java/com/jhonny/rechargesapi/Sale/Sale.java
@@ -1,15 +1,16 @@
 package com.jhonny.rechargesapi.Sale;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import com.jhonny.rechargesapi.Customer.Customer;
 import com.jhonny.rechargesapi.Recharge.Recharge;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,4 +31,10 @@ public class Sale {
     @JoinColumn(name = "customer_id")
     private Customer customer;
     private String person;
+
+    // Add a method to set saleDate to the current date before persisting
+    @PrePersist
+    protected void onCreate() {
+        saleDate = Date.valueOf(LocalDate.now());
+    }
 }
